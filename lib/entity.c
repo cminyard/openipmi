47a4326835ec3ab0dc78a02469e8f6adb72edc84
Corey Minyard <cminyard@mvista.com>
2021-11-29 12:22:11 -0600

    entity: Handle NULL entity in internal_fru_fetch_done()
    In the case the entity is destroyed while doing a FRU fetch, it is
    possible to get internal_fru_fetch_done() called with a NULL entity
    because the entity lookup will fail.  The entity data still exists
    because we get/put it, so pass in the entity in the callback data.
    
    Signed-off-by: Corey Minyard <cminyard@mvista.com>

--- lib/entity.c
+++ lib/entity.c
@@ -1033,6 +1033,12 @@ ent_use_frudev_for_presence(ipmi_entity_t *ent)
 static void
 internal_fru_fetch_done(ipmi_entity_t *ent, void *cb_data)
 {
+    /*
+     * Hold the entity in the cb_data so if the entity is deleted, we
+     * can still get to it to put the value.
+     */
+    ent = cb_data;
+
     ent_lock(ent);
     ent->in_fru_fetch = 0;
     ent_unlock(ent);
@@ -1144,7 +1150,7 @@ i_ipmi_entity_put(ipmi_entity_t *ent)
 	ent->in_fru_fetch = 1;
 	i_ipmi_entity_get(ent);
 	ent_unlock(ent);
-	rv = ipmi_entity_fetch_frus_cb(ent, internal_fru_fetch_done, NULL);
+	rv = ipmi_entity_fetch_frus_cb(ent, internal_fru_fetch_done, ent);
 	if (rv) {
 	    ent_lock(ent);
 	    ent->in_fru_fetch = 0;
@@ -1917,7 +1923,7 @@ presence_changed(ipmi_entity_t *ent, int present)
 	    ent->in_fru_fetch = 1;
 	    i_ipmi_entity_get(ent);
 	    ent_unlock(ent);
-	    rv = ipmi_entity_fetch_frus_cb(ent, internal_fru_fetch_done, NULL);
+	    rv = ipmi_entity_fetch_frus_cb(ent, internal_fru_fetch_done, ent);
 	    if (rv) {
 		ent_lock(ent);
 		ent->in_fru_fetch = 0;
