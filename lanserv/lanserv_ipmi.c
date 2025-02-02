cminyard/openipmi

commit 9cea3152e7c6b6871c8613b861976d4ea489bf34

lanserv: Clear out session data on a close
Make sure that data doesn't show up elsewhere.

Suggested-by: James Henderson <james.henderson@withsecure.com>
Signed-off-by: Corey Minyard <corey@minyard.net>

lanserv/lanserv_ipmi.c
@@ -140,8 +140,12 @@ close_session(lanserv_data_t *lan, session_t *session)
     }
 
     session->active = 0;
-    if (session->authtype <= 4 && session->authdata)
-	ipmi_auths[session->authtype].authcode_cleanup(session->authdata);
+    if (session->authdata) {
+	if (session->authtype <= 4)
+	    ipmi_auths[session->authtype].authcode_cleanup(session->authdata);
+	else
+	    memset(&(session->auth_data), 0, sizeof(auth_data_t));
+    }
     if (session->integh)
 	session->integh->cleanup(lan, session);
     if (session->confh)
@@ -151,6 +155,8 @@ close_session(lanserv_data_t *lan, session_t *session)
 	lan->channel.free(&lan->channel, session->src_addr);
 	session->src_addr = NULL;
     }
+
+    memset(session, 0, sizeof(session_t));
 }
 
 static int

commit fab7af09a3140fd44afcebe6474bf872b75d833c

lanserv: Preserve handle when clearing session data
The handle still has to be there.

Fixes: 9cea3152 ("Clear out session data on a close")

Signed-off-by: Corey Minyard <corey@minyard.net>

lanserv/lanserv_ipmi.c
@@ -128,6 +128,7 @@ static void
 close_session(lanserv_data_t *lan, session_t *session)
 {
     unsigned int i;
+    int old_handle;
 
     for (i = 0; i < LANSERV_NUM_CLOSERS; i++) {
 	if (session->closers[i].close_cb) {
@@ -156,7 +157,9 @@ close_session(lanserv_data_t *lan, session_t *session)
 	session->src_addr = NULL;
     }
 
+    old_handle = session->handle;
     memset(session, 0, sizeof(session_t));
+    session->handle = old_handle;
 }
 
 static int